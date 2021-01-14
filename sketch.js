b1cf7a5ba023af9d8bbf6e6cc3f2a08ee5f90eb8
Meet Patel <bobmeet1308@gmail.com>
2021-01-14 14:14:34 -0600

    Second push

--- sketch.js
+++ sketch.js
@@ -3,5 +3,11 @@ function setup() {
   }
   
   function draw() {
-    background(248,131,121);
+    // background(mouseX,mouseY,mouseX);
+    if (mouseIsPressed) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      ellipse(mouseX, mouseY, 80, 80);
   }
